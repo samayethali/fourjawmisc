1d8056c3cb64be846fda1a3433590d5a5f5d35a9
Sam Ali <11894950+samayethali@users.noreply.github.com>
2022-02-02 10:35:04 +0000

    Update usbreset.sh

--- usbreset.sh
+++ usbreset.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 USBNAME=239a:800f #Put here the usb ID
 LSUSB=$(lsusb | grep --ignore-case $USBNAME)
 FOLD="/dev/bus/usb/"$(echo $LSUSB | cut --delimiter=' ' --fields='2')"/"$(echo $LSUSB | cut --delimiter=' ' --fields='4' | tr --delete ":")
