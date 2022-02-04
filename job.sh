ce060977647ab82cad980ac4decb992e9e1c59af
Sam Ali <11894950+samayethali@users.noreply.github.com>
2022-02-04 16:50:00 +0000

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 tail -20 /var/log/supervisor/lo-fi-collector.out.log > /home/pi/usbreset/tail.txt
 TENS=$(grep -c '10 readings' /home/pi/usbreset/tail.txt)
+echo "Time: $(date). Checking for bad sensorbox" >> /home/pi/usbreset/log.txt
 if [[ $TENS -lt 1 ]]
 then
     bash /home/pi/usbreset/reset.sh
+    echo "Time: $(date). Resetting sensorbox" >> /home/pi/usbreset/log.txt
 fi
