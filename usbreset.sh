d5aa29c5a83ef6013436f6a6e326cae453247e56
Sam Ali <11894950+samayethali@users.noreply.github.com>
2022-02-04 16:14:02 +0000

    Update usbreset.sh

--- usbreset.sh
+++ usbreset.sh
@@ -2,4 +2,4 @@
 USBNAME=239a:800f #Put here the usb ID
 LSUSB=$(lsusb | grep --ignore-case $USBNAME)
 FOLD="/dev/bus/usb/"$(echo $LSUSB | cut --delimiter=' ' --fields='2')"/"$(echo $LSUSB | cut --delimiter=' ' --fields='4' | tr --delete ":")
-sudo ~/usbreset/reset.o $FOLD; #The path where you have the usbreset executable
+sudo /home/pi/usbreset/reset.o $FOLD; #The path where you have the usbreset executable
