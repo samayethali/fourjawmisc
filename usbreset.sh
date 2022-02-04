72d5720531d9f90586c21ed9d940b7276c39373e
Sam Ali <11894950+samayethali@users.noreply.github.com>
2022-02-04 11:31:46 +0000

    Update usbreset.sh

--- usbreset.sh
+++ usbreset.sh
@@ -4,5 +4,4 @@ LSUSB=$(lsusb | grep --ignore-case $USBNAME)
 FOLD="/dev/bus/usb/"$(echo $LSUSB | cut --delimiter=' ' --fields='2')"/"$(echo $LSUSB | cut --delimiter=' ' --fields='4' | tr --delete ":")
 echo $LSUSB
 echo $FOLD
-sudo gcc -o ~/usbreset/reset.o ~/usbreset/reset.c
 sudo ~/usbreset/reset.o $FOLD; #The path where you have the usbreset executable
