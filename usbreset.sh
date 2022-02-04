e32ea746dde6b4e029f3b2726c16b028d77d2004
Sam Ali <11894950+samayethali@users.noreply.github.com>
2022-02-04 15:10:42 +0000

    Update usbreset.sh

--- usbreset.sh
+++ usbreset.sh
@@ -2,6 +2,4 @@
 USBNAME=239a:800f #Put here the usb ID
 LSUSB=$(lsusb | grep --ignore-case $USBNAME)
 FOLD="/dev/bus/usb/"$(echo $LSUSB | cut --delimiter=' ' --fields='2')"/"$(echo $LSUSB | cut --delimiter=' ' --fields='4' | tr --delete ":")
-echo $LSUSB
-echo $FOLD
 sudo ~/usbreset/reset.o $FOLD; #The path where you have the usbreset executable
