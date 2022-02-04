ef8e1fbae0295a2b9d5f31086674b55a41f485fd
Sam Ali <11894950+samayethali@users.noreply.github.com>
2022-02-04 15:29:56 +0000

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tail -20 /var/log/supervisor/lo-fi-collector.out.log > ~/usbreset/tail.txt
-TENS=$(grep -c '10' ~/usbreset/tail.txt)
+TENS=$(grep -c '10 readings' ~/usbreset/tail.txt)
 if [[ $TENS -lt 1 ]]
 then
     bash ~/usbreset/reset.sh
